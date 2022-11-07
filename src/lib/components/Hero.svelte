aac8fa889ca3b11ef8cd158e441bf1a8cf68bf92
0xShy101 <theshy.code@gmail.com>
2022-11-07 10:50:11 +0400

    Responsive Hero

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,16 +1,12 @@
-<section class="bg-base-200 pt-52 pb-36">
+<section class="bg-base-200 py-20 sm:py-32">
 	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-		<div class="hero">
-			<div class="hero-content text-center">
-				<div class="max-w-3xl">
-					<h1 class="text-8xl font-bold">A design agency with a twist</h1>
-					<p class="text-2xl py-8">
-						Design subscriptions to scale your business.
-					</p>
-					<button class="btn btn-primary text-lg">See plans</button>
-                    <p class="py-8">Designs you'll ❤️, guaranteed</p>
-				</div>
-			</div>
-		</div>
+        <div class="flex flex-col gap-8 text-center mx-auto max-w-3xl ">
+                <h1 class="text-6xl sm:text-8xl font-bold">A design agency with a twist</h1>
+                <p class="text-2xl ">
+                    Design subscriptions to scale your business.
+                </p>
+                <button class="btn btn-primary text-lg max-w-[10rem] mx-auto">See plans</button>
+                <p>Designs you'll ❤️, guaranteed</p>
+        </div>
 	</div>
 </section>
