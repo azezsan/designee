0370d1a05cba08680ca2ba589eb9a76057d632c7
0xShy101 <theshy.code@gmail.com>
2022-11-07 10:50:49 +0400

    Better Navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,7 +1,7 @@
 <nav class="bg-base-200">
 	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-		<div class="navbar justify-center">
-			<a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+		<div class="flex justify-center pt-3">
+			<a class="btn btn-ghost normal-case text-2xl" href="/">daisyUI</a>
 		</div>
 	</div>
 </nav>
