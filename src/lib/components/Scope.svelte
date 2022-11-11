4f93115d81ccb713261690675ab5bf1ed116aa9b
0xShy101 <theshy.code@gmail.com>
2022-11-11 23:33:54 +0400

    Scope

---
+++ src/lib/components/Scope.svelte
@@ -0,0 +1,43 @@
+<script>
+    const scope = [
+        "Websites",
+        "Mobile apps",
+        "Logos & branding",
+        "Design systems",
+        "SAAS",
+        "Wireframes",
+        "Stationery",
+        "Pitch decks",
+        "Slide decks",
+        "Direct mail",
+        "Infographics",
+        "Brochures",
+        "Signage",
+        "Brand guides",
+        "Social media graphics",
+        "Billboards",
+        "Trade show banners",
+        "Email graphics",
+        "Resumes",
+        "Business cards",
+        "Packaging",
+        "Blog graphics",
+        "Digital ads",
+        "Icons",
+    ]
+</script>
+
+<section class="bg-base-100 py-20 sm:py-32">
+	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 grid gap-24">
+        <section>
+            <h2 class="text-3xl sm:text-5xl font-bold text-center">Apps, websites, logos & more.</h2>
+        </section>
+        <ul class="grid grid-cols-1 sm:grid-cols-3 gap-8 p-4 sm:p-10 border-base-200 border-8">
+            {#each scope as item}
+                <li class="border-b-4 border-base-200 pb-2">
+                    <h2 class="text-xl font-semibold">{item}</h2>
+                </li>
+            {/each}
+        </ul>
+    </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,17 @@
     import How from "$lib/components/How.svelte";
 	import Features from "$lib/components/Features.svelte"
     import Benefits from "$lib/components/Benefits.svelte";
+    import Protfolio from "$lib/components/Protfolio.svelte";
+    import Scope from "$lib/components/Scope.svelte";
+    import Pricing from "$lib/components/Pricing.svelte";
+    import Faq from "$lib/components/Faq.svelte";
 </script>
 
 <Hero />
 <How />
 <Features />
 <Benefits />
+<Protfolio />
+<Scope />
+<Pricing />
+<Faq />
