85fcd2dcd2199ca2282852f41b49553aad991677
0xShy101 <theshy.code@gmail.com>
2022-11-22 23:40:05 +0400

    Assets

---
+++ src/lib/assets/icons/Arrow.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g id="Arrow">
+		<path
+			id="Body"
+			d="M67.7645 31.3071C59.0224 22.9756 45.8547 22.1647 36.2536 28.6822M68.6209 66.9228C72.9707 62.3586 75.2705 56.5881 75.5478 50.7486M28.2661 37.4733C24.156 44.9939 24.1824 54.1353 28.2661 61.6141M39.5166 72.2771C45.2458 75.0173 51.7803 75.4594 57.7581 73.636"
+			stroke="black"
+			stroke-width="4"
+			stroke-linecap="round"
+		/>
+		<path
+			id="Head"
+			d="M75.2857 28.9996L73.6165 37.2598C73.3977 38.3425 72.3427 39.0428 71.26 38.824L62.9998 37.1548"
+			stroke="black"
+			stroke-width="4"
+			stroke-linecap="round"
+		/>
+	</g>
+</svg>
+
+<style>
+    
+</style>
