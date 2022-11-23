a2d795a175095a5c40e9e62943b62f4b5fe6b685
0xShy101 <theshy.code@gmail.com>
2022-11-24 02:22:24 +0400

    Custom Branding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#000000',
+					secondary: '#E3B7A0',
+					accent: '#EEAF3A',
+					neutral: '#291334',
+					'base-100': '#FFFBF5',
+					'base-200': '#FBF3E6',
+					'base-300': '#FDE7DC',
+					info: '#3ABFF8',
+					success: '#36D399',
+					warning: '#FBBD23',
+					error: '#F87272'
+				}
+			}
+		]
+	},
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
